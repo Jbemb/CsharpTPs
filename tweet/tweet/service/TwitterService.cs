e539daebc27bc9c576271b76ec2022f3811cf59c
Janet BEMBNISTER <janet.bembnister2020@campus-eni.fr>
2020-10-01 16:24:28 +0200

    layout fixed

--- tweet/tweet/service/TwitterService.cs
+++ tweet/tweet/service/TwitterService.cs
@@ -22,15 +22,15 @@ namespace tweet.service
         public List<Tweet> GetTweets()
         {
             List<Tweet> tweets = new List<Tweet>();
-            tweets.Add(new Tweet() { dateCreation = DateTime.Today, identifiant = "1", userId = "1", userName = "janet", userPseudo = "jj", text="qsdfqdfqdffq" });
-            tweets.Add(new Tweet() { dateCreation = DateTime.Today, identifiant = "2", userId = "2", userName = "Kenan", userPseudo = "kk", text = "qsdfqdfqdffqqsdfqsdfqdsf" });
-            tweets.Add(new Tweet() { dateCreation = DateTime.Today, identifiant = "3", userId = "2", userName = "Kenan", userPseudo = "kk", text = "qsdfqdfqdffqertytyuhu" });
-            tweets.Add(new Tweet() { dateCreation = DateTime.Today, identifiant = "4", userId = "2", userName = "Kenan", userPseudo = "kk", text = "zerytzertzrtrtqsdfqdfqdffq" });
-            tweets.Add(new Tweet() { dateCreation = DateTime.Today, identifiant = "5", userId = "3", userName = "julie", userPseudo = "jojo", text = "sdfguiuyqsdfqdfqdffq" });
-            tweets.Add(new Tweet() { dateCreation = DateTime.Today, identifiant = "6", userId = "1", userName = "janet", userPseudo = "jj", text = "qssdfgsdfgsdfgdfqdfqdffq" });
-            tweets.Add(new Tweet() { dateCreation = DateTime.Today, identifiant = "7", userId = "1", userName = "janet", userPseudo = "jj", text = "qsdyuituyityuiuituiuifqdfqdffq" });
-            tweets.Add(new Tweet() { dateCreation = DateTime.Today, identifiant = "8", userId = "1", userName = "janet", userPseudo = "jj", text = "azerretzertreqsdfqdfqdffq" });
-            tweets.Add(new Tweet() { dateCreation = DateTime.Today, identifiant = "9", userId = "1", userName = "janet", userPseudo = "jj", text = "azertjklhkjljklklqsdfqdfqdffq" });
+            tweets.Add(new Tweet() { dateCreation = DateTime.Today, identifiant = "1", userId = "1", userName = "janet", userPseudo = "@jj", text="opopopopopopopooooooooooooooooooooooooooooooooooooooooqsdfqdfqdffq" });
+            tweets.Add(new Tweet() { dateCreation = DateTime.Today, identifiant = "2", userId = "2", userName = "Kenan", userPseudo = "@kk", text = "jkljkljkkkkkkkkkkkkllllllllllllllllllllllllllllllllllllllqsdfqdfqdffqqsdfqsdfqdsf" });
+            tweets.Add(new Tweet() { dateCreation = DateTime.Today, identifiant = "3", userId = "2", userName = "Kenan", userPseudo = "@kk", text = "q'''''''''''hjjjjjjjjjjjjjjhfffffffffffffffffffsdfqdfqdffqertytyuhu" });
+            tweets.Add(new Tweet() { dateCreation = DateTime.Today, identifiant = "4", userId = "2", userName = "Karen", userPseudo = "@kiki", text = "fdddddddddddddddddddyyyyyyyyzerytzertzrtrtqsdfqdfqdffq" });
+            tweets.Add(new Tweet() { dateCreation = DateTime.Today, identifiant = "5", userId = "3", userName = "julie", userPseudo = "@jojo", text = "hhhhhhhhhhhhhhhhhhhhhfsdfguiuyqsdfqdfqdffq" });
+            tweets.Add(new Tweet() { dateCreation = DateTime.Today, identifiant = "6", userId = "1", userName = "janet", userPseudo = "@jj", text = "bbbbbbbbbbbbbbbbbvb,vbn,vbn,vbn,vbn,vbn,vbn,vbn,vbn,vbn,vbn,vbn,qssdfgsdfgsdfgdfqdfqdffq" });
+            tweets.Add(new Tweet() { dateCreation = DateTime.Today, identifiant = "7", userId = "1", userName = "jan", userPseudo = "@jaja", text = "nnnnnnnnnnnnnnnnnyuituioyipuyiopyuioyuioyuioyuioqsdyuituyityuiuituiuifqdfqdffq" });
+            tweets.Add(new Tweet() { dateCreation = DateTime.Today, identifiant = "8", userId = "1", userName = "janet", userPseudo = "@jj", text = "azerretzertreqsdfqdfqdffq" });
+            tweets.Add(new Tweet() { dateCreation = DateTime.Today, identifiant = "9", userId = "1", userName = "janet", userPseudo = "@jj", text = "azertjklhkjljklklqsdfqdfqdffq" });
             return tweets;
         }
 
